96daf67e7ff7453f409adcd093d36960732200f3
Nour Chaibi <nour.chaibi@esprit.tn>
2026-06-11 18:25:28 +0100

    Update AuthUtils.java

--- javaProject/src/main/java/utils/AuthUtils.java
+++ javaProject/src/main/java/utils/AuthUtils.java
@@ -9,19 +9,5 @@ public class AuthUtils {
         return BCrypt.checkpw(plainPassword, hashedPassword);
     }
 
-    /*public static String hashPassword(String password) {
-    try {
-        MessageDigest md = MessageDigest.getInstance("SHA-256"); // Or any other algorithm
-        byte[] hash = md.digest(password.getBytes());
-        StringBuilder hexString = new StringBuilder();
-        for (byte b : hash) {
-            String hex = Integer.toHexString(0xff & b);
-            if(hex.length() == 1) hexString.append('0');
-            hexString.append(hex);
-        }
-        return hexString.toString();
-    } catch (NoSuchAlgorithmException e) {
-        throw new RuntimeException("Hashing algorithm not found", e);
-    }
-}*/
+   
 }
